828df6ca8a5d14cf82b7588a265059719660b96a
HeymeCoraspe <heyme.coraspe@gmail.com>
2023-06-26 20:21:57 -0300

    agrego turno computadora y funcionalida de botones

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,8 +1,20 @@
 //creando el mazo
-
 let mazo=[];
-const tipos=["C", "D", "H", "S"]; 
-      especiales=["A", "J", "Q", "K"];
+const tipos=["C", "D", "H", "S"]; especiales=["A", "J", "Q", "K"];
+let puntosJugador=0; 
+let puntosComputadora=0;
+
+
+//referencias html
+ const btnPedirCarta= document.querySelector ("#btnPedirCarta");
+ const btnDetener= document.querySelector("#btnDetener"); 
+ const btnNuevoJuego= document.querySelector("#btnNuevoJuego");
+
+ const puntosHTML= document.querySelectorAll("small"); 
+
+ const divCartasJugador=document.querySelector("#jugador-cartas");
+ const divCartasComputadora= document.querySelector("#computadora-cartas");
+
 
 const crearMazo= ()=>{
     for(let i=2; i<=10; i++){
@@ -16,8 +28,7 @@ const crearMazo= ()=>{
         }
     }
     mazo= _.shuffle(mazo); //librería underscore: _.shuffle devuelve el arreglo desordenado
-    console.log(mazo);
-    return mazo
+    return mazo;
 }
 crearMazo();
 
@@ -29,25 +40,100 @@ const pedirCarta=()=>{
         throw "No hay más cartas en el mazo"
     }
     const carta= mazo.pop(); //para remover la última carta del mazo
-
-    console.log(mazo);
-    console.log(carta);
-    return carta
+    return carta;
 }
 pedirCarta();
-
 //asignando valores a las cartas
 const valorCarta =(carta)=>{
         const valor= carta.substring(0,carta.length -1);
         return(isNaN(valor))?(valor==="A") ? 11:10 : valor*1;
-  
 }
-const valor= valorCarta(pedirCarta());
-console.log({valor});
- //sigue Introducción al DOM y su manipulación
+//Turno computadora
+const turnoComputadora=(puntosMinimos)=>{
+    do{
+        const carta= pedirCarta();
 
+        puntosComputadora= puntosComputadora+ valorCarta(carta);
+        puntosHTML[1].innerText=puntosComputadora;
 
+        const imgCarta=document.createElement("img");
+        imgCarta.src=`assets/cartas/${carta}.png`;// `${}` agregar código JS a la imagen
+        imgCarta.classList.add("carta");
+        divCartasComputadora.append(imgCarta);
+        console.log(carta);
 
+        if(puntosMinimos>21){
+            break;
+        }
+    } while((puntosComputadora < puntosMinimos) && puntosMinimos <= 21 );
+
+    setTimeout(()=>{ //if se evalua y ejecuta cuando termina el ciclo
+        if(puntosComputadora===puntosMinimos){
+            alert("Nadie gana =(");
+        } else if(puntosMinimos>21){
+            alert("Computadora gana =(");
+        } else if(puntosComputadora>21){
+            alert("Ganaste =D");
+        } else{
+            alert("Computadora gana =(");
+        }
+    }, 30);
+}
+//eventos click
+btnPedirCarta.addEventListener("click", ()=>{ //al hacer click en el boón, se realiza la acción dentro de la función
+   const carta= pedirCarta();
+
+   puntosJugador= puntosJugador+ valorCarta(carta);
+   puntosHTML[0].innerText=puntosJugador;
+
+   const imgCarta=document.createElement("img");
+   imgCarta.src=`assets/cartas/${carta}.png`;// `${}` agregar código JS a la imagen
+   imgCarta.classList.add("carta");
+   divCartasJugador.append(imgCarta);
+   console.log(carta);
+
+   if(puntosJugador>21){
+        console.warn("Perdiste =(");
+        btnPedirCarta.disabled=true;
+        btnDetener.disabled=true;
+        turnoComputadora(puntosJugador);
+
+   } else if(puntosJugador ===21){
+        console.warn("21 =D");
+        btnPedirCarta.disabled=true;
+        btnDetener.disabled=true;
+        turnoComputadora(puntosJugador);
+   }
+});
+
+btnDetener.addEventListener("click",()=>{
+    btnPedirCarta.disabled=true;
+    btnDetener.disabled=true;
+
+    turnoComputadora(puntosJugador);
+})
+
+btnNuevoJuego.addEventListener("click", ()=>{
+     console.clear();
+     
+    mazo=[];
+    mazo=crearMazo();
+
+    puntosJugador=0;
+    puntosComputadora=0;
+
+    puntosHTML[0].innerText=0;
+    puntosHTML[1].innerText=0;
+
+    divCartasJugador.innerText="";
+    divCartasComputadora.innerText="";
+
+    btnPedirCarta.disabled=false;
+    btnDetener.disabled=false;
+    console.log(mazo);
+})
+
+// empieza sección 6
 
 
 
